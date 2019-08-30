813df9548e42d12ea4d35be9a86dddeb9e914ef3
Eduardo Betim Rosa <edu.betim@aol.com>
2019-08-30 08:22:35 -0300

    Add files via upload

---
+++ URI-1001/1001.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+        
+        int intA = teclado.nextInt();
+        int intB = teclado.nextInt();
+        int intX = A + B;
+        
+        System.out.println("X = " + intX);
+    }
+ 
+}
