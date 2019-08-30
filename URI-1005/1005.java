e466e15cd733d1a25ff0644dfe3481144a86ccb3
Eduardo Betim Rosa <edu.betim@aol.com>
2019-08-30 08:25:02 -0300

    Add files via upload

---
+++ URI-1005/1005.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        
+		Scanner teclado = new Scanner(System.in);
+        
+		double nota1 = teclado.nextDouble();
+		double nota2 = teclado.nextDouble();
+		double media = ((nota1 * 3.5) + (nota2 * 7.5)) / 11;
+        
+		System.out.printf("MEDIA = %.5f", media);
+		System.out.println();
+	}
+}
